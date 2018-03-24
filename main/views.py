3d49fb10db6dd915609f92c94d717a82d5c90ee9
laveesingh <lavees96@gmail.com>
2018-03-24 23:22:45 +0530

    wrote first view for creating new post

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+from main.models import Post
 # Create your views here.
+
+def create_post(request):
+    body = request.POST
+    title = body.get('title')
+    description = body.get('description')
+    content = body.get('content')
+    flag = 0
+    if not title or not description or not content:
+        print("Request is missing data")
+        flag = 1
+    else:
+        Post.objects.create(
+                title=title,
+                description=description,
+                content=content
+                )
+    return JsonResponse({'message': 'successful'})
