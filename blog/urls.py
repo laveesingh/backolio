8846d85451e09da1fb50784ff6223cb1332eaca3
laveesingh <lavees96@gmail.com>
2018-05-09 14:53:33 +0530

    added urls for update and delete view

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from blog import views
 
 urlpatterns = [
     url(r'^post/create/', views.create_post),
+    url(r'^post/update/', views.update_post),
+    url(r'^post/delete/(?P<pk>\d+)/', views.delete_post),
     url(r'^post/list/', views.get_posts),
     url(r'^post/(?P<pk>\d+)/', views.get_post),
     url(r'^tools/cfreport/', views.generate_cf_report)
